16d763d6e767a0d26cdee9b05055574d1999d912
csjames03 <csjames0303@gmail.com>
2025-09-02 22:44:35 +0800

    chore: Initial Codebase for codemaster

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,9 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
+
+const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || "0.0.0.0";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -60,12 +63,7 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
+  app.listen(5000, HOST, () => {
+    console.log(`Server running at http://${HOST}:${PORT}`);
   });
 })();
